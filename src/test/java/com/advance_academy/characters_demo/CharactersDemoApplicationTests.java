4bfdc8b5cae652c3ff1c5c18c1c97e557dd9279f
gvnestorov <gvnestorov@gmail.com>
2021-05-19 11:46:59 +0300

    Initial commit

---
+++ src/test/java/com/advance_academy/characters_demo/CharactersDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advance_academy.characters_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharactersDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
